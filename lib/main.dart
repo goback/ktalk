b957b28ed804f191bc2af7ab1c4ac3f48f7d656f
moo <excusme@naver.com>
2024-04-03 10:20:56 +0900

    Flutter 프로젝트와 Firebase 프로젝트 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ktalk/common/utils/logger.dart';
+import 'package:ktalk/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
