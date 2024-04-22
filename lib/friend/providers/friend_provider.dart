45bed69ec1fe4e198ff66fb19c641c5c572474d9
moo <excusme@naver.com>
2024-04-22 09:36:53 +0900

    상태관리 데이터 삭제

--- lib/friend/providers/friend_provider.dart
+++ lib/friend/providers/friend_provider.dart
@@ -1,7 +1,17 @@
 import 'package:flutter_contacts/contact.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ktalk/auth/providers/auth_provider.dart';
 import 'package:ktalk/friend/repositories/friend_repository.dart';
 
-final getFriendListProvider = FutureProvider<List<Contact>>(
-  (ref) => ref.watch(friendRepositoryProvider).getFriendList(),
+final getFriendListProvider = AutoDisposeFutureProvider<List<Contact>>(
+  (ref) {
+    final link = ref.keepAlive();
+    ref.listen(authStateProvider, (previous, next) {
+      if (next.value == null) {
+        link.close();
+      }
+    });
+
+    return ref.watch(friendRepositoryProvider).getFriendList();
+  },
 );
