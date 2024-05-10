099ed15bb39a072a409c09537923fe8c2bbf7fab
moo <excusme@naver.com>
2024-05-10 14:31:08 +0900

    채팅방 나가기 위젯

--- lib/chat/screens/chat_list_screen.dart
+++ lib/chat/screens/chat_list_screen.dart
@@ -1,9 +1,11 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:ktalk/chat/providers/chat_provider.dart';
 import 'package:ktalk/chat/screens/chat_screen.dart';
+import 'package:ktalk/common/utils/locale/generated/l10n.dart';
 import 'package:ktalk/common/utils/logger.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
@@ -18,51 +20,66 @@ class _ChatListScreenState extends ConsumerState<ChatListScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 15),
       child: ref.watch(chatListProvider).when(
         data: (data) {
           context.loaderOverlay.hide();
-          return ListView.separated(
+          return ListView.builder(
             itemCount: data.length,
-            separatorBuilder: (context, index) => const SizedBox(height: 10),
             itemBuilder: (context, index) {
               final chatModel = data[index];
               final userModel = chatModel.userList[1];
-              return ListTile(
-                onTap: () {
-                  ref.read(chatProvider.notifier).enterChatFromChatList(
-                        chatModel: chatModel,
-                      );
-                  Navigator.pushNamed(
-                    context,
-                    ChatScreen.routeName,
-                  ).then((value) => ref.invalidate(chatProvider));
-                },
-                leading: CircleAvatar(
-                  backgroundImage: userModel.photoURL == null
-                      ? const ExtendedAssetImageProvider(
-                          'assets/images/profile.png') as ImageProvider
-                      : ExtendedNetworkImageProvider(userModel.photoURL!),
-                  radius: 30,
+              return Slidable(
+                startActionPane: ActionPane(
+                  motion: const DrawerMotion(),
+                  extentRatio: 0.25,
+                  children: [
+                    SlidableAction(
+                      onPressed: (context) {},
+                      backgroundColor: Colors.red,
+                      icon: Icons.exit_to_app_rounded,
+                      label: S.current.exit,
+                    ),
+                  ],
                 ),
-                title: Text(
-                  userModel.displayName,
-                  style: const TextStyle(fontSize: 18),
-                ),
-                subtitle: Text(
-                  chatModel.lastMessage,
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                  ),
-                ),
-                trailing: Text(
-                  DateFormat.Hm().format(chatModel.createAt.toDate()),
-                  style: const TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: ListTile(
+                    onTap: () {
+                      ref.read(chatProvider.notifier).enterChatFromChatList(
+                            chatModel: chatModel,
+                          );
+                      Navigator.pushNamed(
+                        context,
+                        ChatScreen.routeName,
+                      ).then((value) => ref.invalidate(chatProvider));
+                    },
+                    leading: CircleAvatar(
+                      backgroundImage: userModel.photoURL == null
+                          ? const ExtendedAssetImageProvider(
+                              'assets/images/profile.png') as ImageProvider
+                          : ExtendedNetworkImageProvider(userModel.photoURL!),
+                      radius: 30,
+                    ),
+                    title: Text(
+                      userModel.displayName,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Text(
+                      chatModel.lastMessage,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    trailing: Text(
+                      DateFormat.Hm().format(chatModel.createAt.toDate()),
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
+                      ),
+                    ),
                   ),
                 ),
               );
